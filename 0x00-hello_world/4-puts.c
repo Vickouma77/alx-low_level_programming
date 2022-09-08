85b617cc2a3304dcbe0ffce8a0a3de7b686aca68
vickouma77 <vickprestone20@gmail.com>
2022-09-08 02:06:20 -0700

    white space delete

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
- * main -Entry point
+ * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
-{        
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
