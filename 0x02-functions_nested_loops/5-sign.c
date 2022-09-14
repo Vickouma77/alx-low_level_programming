55e178c48967d8e58bbea8d6b3995235ed9b3835
vickouma77 <vickprestone20@gmail.com>
2022-09-14 00:32:43 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - Determines if the input number
+ * greater, iqual or less than zero.
+ *
+ * @n: The input number as an integer.
+ *
+ * Return: 1 if n is greater than zero. 0 is zero.
+ * n is equal to zero.-1 if otherwise
+ */
+
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+_putchar('0');
+return (0);
+}
