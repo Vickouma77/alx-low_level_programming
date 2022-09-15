146918752bc0a90434faf55c3deef3e7241fc7c4
vickouma77 <vickprestone20@gmail.com>
2022-09-15 08:37:02 -0700

    task1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,8 @@
-#include "holberton.h"
+#include "main.h"
+#include <stdio.h>
 
 /**
- * _isdigit - checks if parameter is a number between 0 to 9.
+ * _isdigit - checks if a number is between 0 to 9.
  * @c: input number.
  * Return: 1 if is a number (0 to 9), 0 in other case.
  */
