84f08da3d3e39fb8519d51b758b45d36ebd6ed08
vickouma77 <vickprestone20@gmail.com>
2022-09-19 12:44:48 -0700

    Task0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main- takes a pointer to an int as parameter 
+ *
+ *@n: integer pointer
+ *
+ * Return: 0
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
