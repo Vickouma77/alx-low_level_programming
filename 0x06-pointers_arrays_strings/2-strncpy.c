07c682d823bdc863b8ce7c19a85104b0c6741a2a
vickouma77 <vickprestone20@gmail.com>
2022-09-22 03:03:15 -0700

    Task2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncpy- copies strings
+ * @dest: destination
+ * @src: destination
+ * @n: integer
+ *
+ * Return: the pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
