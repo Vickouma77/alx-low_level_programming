2d612974acf23b649a0eaf0dcd612c72e6703410
vickouma77 <vickprestone20@gmail.com>
2022-09-26 13:22:48 -0700

    fix Task5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,21 +11,22 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	char *h;
-	char *n;
+	char *bhaystack;
+	char *pneedle;
 
 	while (*haystack != '\0')
 	{
-		h = haystack;
-		n = needle;
-		while (*haystack != '\0' && *n != '\0' && *haystack == *n)
+		bhaystack = haystack;
+		pneedle = needle;
+
+		while (*haystack != '\0' && *pneedle != '\0' && *haystack == *pneedle)
 		{
 			haystack++;
-			n++;
+			pneedle++;
 		}
-		if (!n)
-			return (h);
-		haystack = h + 1;
+		if (!*pneedle)
+			return (bhaystack);
+		haystack = bhaystack + 1;
 	}
 	return (0);
 }
