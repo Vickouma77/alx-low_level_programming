9200c5183fc4078283a5971044c1cb49258525ff
vickouma77 <vickprestone20@gmail.com>
2022-09-26 13:34:28 -0700

    update main file

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef _main_h_
 #define _main_h_
 
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
