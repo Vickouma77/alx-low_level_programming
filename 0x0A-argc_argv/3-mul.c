05853b41faa4907d0cec44615cfb6dbfa9c45e99
vickouma77 <vickprestone20@gmail.com>
2022-09-30 05:17:23 -0700

    Task3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,9 +15,6 @@ int main(int argc, argv[])
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-		return (0);
-	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+                return (0);
 }
