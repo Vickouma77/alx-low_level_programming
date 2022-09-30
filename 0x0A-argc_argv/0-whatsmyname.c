fee1705f1235bae6e27957cf9490344258a8773c
vickouma77 <vickprestone20@gmail.com>
2022-09-30 04:06:10 -0700

    Task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main-  prints name followed by a new line
+ * @argc: argument count
+ * @argv: array of argument
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%d\n", argv[0]);
+	return (0);
+}
