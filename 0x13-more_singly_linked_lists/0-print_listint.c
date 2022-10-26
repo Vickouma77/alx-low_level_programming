3a854293f3887021c001043f41938709a82a0fa0
vickouma77 <vickprestone20@gmail.com>
2022-10-26 08:13:46 -0700

    correction

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,7 +10,7 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t node = 0;
+	size_t nnodes = 0;
 
 	while (h != NULL)
 	{
@@ -19,5 +19,5 @@ size_t print_listint(const listint_t *h)
 
 		node++
 	}
-	return (node);
+	return (nnodes);
 }
