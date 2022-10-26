30b839e5c0e8c6f6a4635017dbe393bf246606bd
vickouma77 <vickprestone20@gmail.com>
2022-10-26 07:28:20 -0700

    including _putchar

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 
+int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
