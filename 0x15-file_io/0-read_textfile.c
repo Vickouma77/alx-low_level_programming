2dc41d5a94ab22a70f550de2b787bff21f804aa1
vickouma77 <vickprestone20@gmail.com>
2022-11-02 09:10:21 -0700

    Task0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,7 +11,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t nrd, nwr;
-	char *buff;
+	char *buf;
 
 	if (!filename)
 		return (0);
@@ -30,7 +30,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	close(fd);
 
-	free(buff);
+	free(buf);
 
 	return (nwr);
 }
