3b8cd32792d0f8d56c143cb17615c1f02705602b
vickouma77 <vickprestone20@gmail.com>
2022-11-02 09:12:34 -0700

    task0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -26,7 +26,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	nrd = read(fd, buff, letters);
-	nwr = write(STDOUT_FILENO, buff, nrd);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
 	close(fd);
 
