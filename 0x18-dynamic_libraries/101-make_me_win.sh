b5fa39529b6a242700a8592010b742404df0587a
vickouma77 <vickprestone20@gmail.com>
2022-12-13 16:03:08 -0800

    changing script

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/Vickouma77/alx-low_level_programming/master/0x18-dynamic_libraries/lib101-make_me_win.so
+wget -P /tmp/ https://github.com/Vickouma77/alx-low_level_programming/tree/master/0x18-dynamic_libraries/lib101-make_me_win.so
 export LD_PRELOAD=/tmp/lib101-make_me_win.so
