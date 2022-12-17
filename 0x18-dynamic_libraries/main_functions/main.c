3f61368ecf3221ac65a86a156efac81890823c36
vickouma77 <vickprestone20@gmail.com>
2022-12-17 07:24:18 -0800

    adding C functions for python

---
+++ 0x18-dynamic_libraries/main_functions/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the codes
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+	printf("%d\n", _strlen("school"));
+	return (EXIT_SUCCESS);
+}
