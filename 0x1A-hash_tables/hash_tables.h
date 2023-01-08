6baa83fa59cf7521093800e3231624212380fa28
vickouma77 <vickprestone20@gmail.com>
2023-01-08 06:55:36 -0800

    updating header file

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,9 +1,9 @@
-#ifndef _HASH_
-#define _HASH_
+#ifndef __HASH__
+#define __HASH__
 
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 /**
  * struct shash_node_s - Node of a sorted hash table
@@ -17,11 +17,11 @@
  */
 typedef struct shash_node_s
 {
-     char *key;
-     char *value;
-     struct shash_node_s *next;
-     struct shash_node_s *sprev;
-     struct shash_node_s *snext;
+	char *key;
+	char *value;
+	struct shash_node_s *next;
+	struct shash_node_s *sprev;
+	struct shash_node_s *snext;
 } shash_node_t;
 
 /**
@@ -36,10 +36,10 @@ typedef struct shash_node_s
  */
 typedef struct shash_table_s
 {
-     unsigned long int size;
-     shash_node_t **array;
-     shash_node_t *shead;
-     shash_node_t *stail;
+	unsigned long int size;
+	shash_node_t **array;
+	shash_node_t *shead;
+	shash_node_t *stail;
 } shash_table_t;
 
 /**
@@ -52,9 +52,9 @@ typedef struct shash_table_s
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -67,19 +67,36 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
+/* 0-hash_table_create.c */
 hash_table_t *hash_table_create(unsigned long int size);
+
+/* 1-djb2.c */
 unsigned long int hash_djb2(const unsigned char *str);
+
+/* 2-key_index.c */
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
+
+/* 3-hash_table_set.c */
+hash_node_t *add_n_hash(hash_node_t **, const char *, const char *);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+
+/* 4-hash_table_get.c */
 char *hash_table_get(const hash_table_t *ht, const char *key);
+
+/* 5-hash_table_print.c */
 void hash_table_print(const hash_table_t *ht);
+
+/* 6-hash_table_delete.c */
 void hash_table_delete(hash_table_t *ht);
 
+/* 100-sorted_hash_table.c */
 shash_table_t *shash_table_create(unsigned long int size);
+shash_node_t *add_n_shash(shash_node_t **, const char *, const char *);
+void add_i_shash(shash_table_t *ht, shash_node_t *new);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
 char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_print(const shash_table_t *ht);
