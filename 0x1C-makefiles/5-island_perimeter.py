7df9b650fe88876172c696645e4831e56645f91e
vickouma77 <Vickprestone20@gmail.com>
2023-02-23 17:39:24 +0300

    island perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Define functions of an island perimeter"""
+
+def island_perimeter(grid):
+    """Returns perimeter of an Island
+
+    Args:
+       grid(list): List of integers
+    Returns:
+       The perimeter of island in grid
+    """
+
