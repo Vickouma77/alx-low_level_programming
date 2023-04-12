2256cd5bcb73aa23bf2198552116b66484914021
vickouma77 <Vickprestone20@gmail.com>
2023-04-12 13:08:10 +0300

    binary search

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,18 @@
+#include "search_algo.h"
+
+/**
+ * binary_search - searches for value in a sorted
+ * array of integers using Binary search algorithm
+ *
+ * @array: pointer to first array to search
+ * @size: number of elements in array
+ * @value: value to search for
+ *
+ * Return: index where value is located or -1 if array is NULL
+ */
+int binary_search(int *array, size_t size, int value);
+{
+	int i;
+
+	if (arry == NULL)
+		return (-1);
