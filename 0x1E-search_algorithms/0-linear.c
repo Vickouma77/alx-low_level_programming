2480b6a4cdf8a5a8019f1e7ac0758fa0600764ea
vickouma77 <Vickprestone20@gmail.com>
2023-04-12 12:23:40 +0300

    adding imdentation

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,28 +2,28 @@
 
 /**
  * linear_search - Searches for a value in an array of integers
- *                 using the Linear search algorithm.
+ * using the Linear search algorithm.
  *
  * @array: A pointer to the first element of the array to search in.
  * @size: The number of elements in the array.
  * @value: The value to search for.
  *
  * Return: The first index where the value is located,
- *         or -1 if the value is not present in the array
- *         or if the array is NULL.
+ * or -1 if the value is not present in the array or if the array is NULL.
  */
 int linear_search(int *array, size_t size, int value)
 {
-    if (array == NULL)
-        return (-1);
+	int i;
 
-    for (size_t i = 0; i < size; i++)
-    {
-        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+	if (array == NULL)
+		return (-1);
 
-        if (array[i] == value)
-            return (i);
-    }
+	for (size_t i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 
-    return (-1);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
