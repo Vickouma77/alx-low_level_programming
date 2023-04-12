ea17611f9c09114693ad293cedf6c770667578c1
vickouma77 <Vickprestone20@gmail.com>
2023-04-12 10:22:56 +0300

    updating linear search algorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -19,6 +19,8 @@ int linear_search(int *array, size_t size, int value)
 
     for (size_t i = 0; i < size; i++)
     {
+        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+
         if (array[i] == value)
             return (i);
     }
